ebbdcd260944dfe3c092a705b0b88291f0b03521
Obhi, Jagpreet <jagpreet_obhi@manulife.com>
2017-07-16 12:49:14 -0400

    minor updates

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # quick-book-app
+
 Loopback app consuming Quick Apps with OAuth
+
+## References
+
+https://github.com/intuit/intuit-developer-nodejs
+https://github.com/puttareddy/quick-books-app
--- server/boot/quick-books.js
+++ server/boot/quick-books.js
@@ -3,63 +3,77 @@
 const QuickBooks = require('node-quickbooks');
 const request = require('request');
 const qs = require('querystring');
-const app = require('../server');
 
 module.exports = app => {
+
   const qbConfig = app.get('qb');
-  const consumerKey = qbConfig.consumerKey;
-  const consumerSecret = qbConfig.consumerSecret;
-  const companyId = qbConfig.companyId;
+  const consumer_key = qbConfig.consumerKey;
+  const consumer_secret = qbConfig.consumerSecret;
+
+  /**
+   * This route authenticates the application with the Quick Books.
+   * A callback URL is provided for the Quick Book to revert with oauth token and secret.
+   *
+   * @param  {string} /authenticate - Route
+   * @param  {object} req - HttpRequest
+   * @param  {object} res - HttpResponse
+   */
   app.get('/authenticate', (req, res) => {
     var postBody = {
       url: QuickBooks.REQUEST_TOKEN_URL,
       oauth: {
-        'callback': 'http://localhost:' + app.get('port') + '/callback',
-        'consumer_key': consumerKey,
-        'consumer_secret': consumerSecret,
-      },
+        callback: `${req.protocol}://${req.hostname}:${app.get('port')}/callback`,
+        consumer_key,
+        consumer_secret
+      }
     };
     request.post(postBody, (error, response, body) => {
       const requestToken = qs.parse(body);
-      req.session['oauth_token_secret'] = requestToken.oauth_token_secret;
+      req.session.oauth_token_secret = requestToken.oauth_token_secret;
       res.redirect(QuickBooks.APP_CENTER_URL + requestToken.oauth_token);
     });
   });
+
+  /**
+   * This callback is used to receive the oauth token and secret from the Quick Book.
+   *
+   * @param  {string} '/callback' - Route
+   * @param  {object} req - HttpRequest
+   * @param  {object} res - HttpResponse
+   */
   app.get('/callback', (req, res) => {
     const postBody = {
       url: QuickBooks.ACCESS_TOKEN_URL,
       oauth: {
-        'consumer_key': consumerKey,
-        'consumer_secret': consumerSecret,
-        'token': req.query.oauth_token,
-        'token_secret': req.session.oauth_token_secret,
-        'verifier': req.query.oauth_verifier,
-        'realmId': req.query.realmId,
-      },
+        consumer_key,
+        consumer_secret,
+        token: req.query.oauth_token,
+        token_secret: req.session.oauth_token_secret,
+        verifier: req.query.oauth_verifier,
+        realmId: req.query.realmId
+      }
     };
+    // This call is made to obtain the Access Token
     request.post(postBody, (error, response, body) => {
-
-      let accessToken = qs.parse(body);
-      const token = accessToken.oauth_token;
-      const secret = accessToken.oauth_token_secret;
+      const verifiedAccessToken = qs.parse(body);
+      const token = verifiedAccessToken.oauth_token;
+      const secret = verifiedAccessToken.oauth_token_secret;
       const companyId = postBody.oauth.realmId;
-
-      const QB = new QuickBooks(consumerKey,
-        consumerSecret,
+      const QB = new QuickBooks(consumer_key,
+        consumer_secret,
         token,
         secret,
         companyId,
         true, // Sandbox
         true); // Debug
       QB.findCustomers({
-          limit: 2,
+          limit: 2
         },
         (err, results) => {
           const customerList = results.QueryResponse.Customer;
           app.models.Customer.destroyAll({});
           customerList.forEach(
             value => {
-              console.log(value);
               app.models.Customer.create(value);
             });
         });
