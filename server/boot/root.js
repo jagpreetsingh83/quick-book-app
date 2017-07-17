13ef662ccea95db774006d62c952c8e4817d76b7
Obhi, Jagpreet <jagpreet_obhi@manulife.com>
2017-07-16 22:01:35 -0400

    Added static page and healthcheck

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,8 +1,24 @@
 'use strict';
 
-module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+const {
+  name,
+  version
+} = require('../../package');
+
+const status = () => {
+  var started = new Date();
+  return function (req, res) {
+    res.send({
+      started: started,
+      uptime: (Date.now() - Number(started)) / 1000,
+      name,
+      version
+    });
+  };
+};
+
+module.exports = function (server) {
+  const router = server.loopback.Router();
+  router.get('/healthcheck', status());
   server.use(router);
 };
